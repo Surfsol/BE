9997c480c2f2f772982972d08e9158a757e9ae4f
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-18 12:14:48 -0800

    Made a notes file for Russ, updated server js file

---
+++ NOTES FOR RUS.md
@@ -0,0 +1,9 @@
+Hello Russ! These are notes I have for you as I edit this shared back-end
+
+### Files Changed
+server.js - switched the initial GET request to something that can be parsed by a browser
+
+### Files Added
+
+### Files Removed
+I don't plan on removing any files, but just in case, I will leave this here!
--- api/server.js
+++ api/server.js
@@ -16,6 +16,10 @@ server.use('/users', usersRouter)
 // server.use('/subreddit', subRouter)
 // server.use('/post', postRouter)
 
+/* RUSS */
+// I switched the get message to something that can be parsed by a web browser 
+// This will aid us in hosting on Heroku (easier to check to see if the app is working)
+
 server.get('/', (req, res) => { res.status(200).json({hello: 'Web 23'})})
 
 module.exports = server;
