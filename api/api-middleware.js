cbd72ad7505ae5ff085867b46e9907bc4072f1aa
Surfsol <surfsol@protonamil.com>
2019-11-18 19:00:10 -0300

    add cors

--- api/api-middleware.js
+++ api/api-middleware.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors')
 
 
 const server = express();
 
-server.use(helmet());
-server.use(express.json());
 
 
 
 module.exports = server => {
     server.use(helmet());
     server.use(express.json());
+    server.use(cors())
   };
