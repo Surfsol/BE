1ed0c74d4ce8ff05741678b48e383baddaf18e11
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-19 09:23:27 -0800

    Currently working on register issue

--- routes-models/users/users-router.js
+++ routes-models/users/users-router.js
@@ -29,7 +29,7 @@ router.post('/register', (req, res)=>{
             res.status(201).json({saved, token})
         })
         .catch(err => {
-            res.status(500).json(err)
+            res.status(500).json({error: err})
         })
 })
 
