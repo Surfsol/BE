6e86c9fd6e3a08d96607d49b291adffa93388b5d
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-21 12:52:37 -0800

    Finalized testing

--- routes-models/subreddit/sub.spec.js
+++ routes-models/subreddit/sub.spec.js
@@ -18,16 +18,19 @@ describe('This is to set up our test environment', () => {
 describe('This is testing the subreddit route', () => {
     describe('This gets all of the subreddits', () => {
         it('gets a response status of 200', async () => {
+            await request(server).post('/users/register').send({username: "Jest", password: "pass"})
             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
             expect(response.status).toBe(200)
         })
         it('should return an array', async () => {
+            await request(server).post('/users/register').send({username: "Jest", password: "pass"})
             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
             expect(JSON.stringify(response.body)).toMatch(`${[]}`)
         })
         it('should return a piece of JSON', async () => {
+            await request(server).post('/users/register').send({username: "Jest", password: "pass"})
             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
             expect(response.type).toMatch(/json/i)
