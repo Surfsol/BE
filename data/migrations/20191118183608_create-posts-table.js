27016f47d91894fe1d6d63b142f7a89cf27b6679
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-21 12:36:31 -0800

    Updated functionality for post router

--- data/migrations/20191118183608_create-posts-table.js
+++ data/migrations/20191118183608_create-posts-table.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('posts', tbl => {
         tbl.increments('id'); //primary key
-        tbl.string('title')
+        tbl.text('title')
             .notNullable()
-        tbl.string('content')
+        tbl.text('content')
             .notNullable()
         //foreign key, on knexfile.js, pool: is used for fk enforcement
         tbl.integer('user_id')
@@ -19,25 +19,18 @@ exports.up = function(knex) {
         tbl.boolean('link')
             .notNullable()
             .defaultTo(0)
-        tbl.string('first')
-        tbl.string('first_subscribers')
-        tbl.string('first_description')
-        tbl.string('first_url')
-        tbl.string('second')
-        tbl.string('second_subscribers')
-        tbl.string('second_description')
-        tbl.string('second_url')
-        tbl.string('third')
-        tbl.string('third_subscribers')
-        tbl.string('third_description')
-        tbl.string('third_url')
-        tbl.integer('subreddit_id')
-            .unsigned() //integer must be positive, necessary in many dbs
-            //referenced on users
-            .references('id')
-            .inTable('subreddit')
-            .onUpdate('CASCADE')
-            .onDelete('CASCADE')
+        tbl.text('first')
+        tbl.text('first_subscribers')
+        tbl.text('first_description')
+        tbl.text('first_url')
+        tbl.text('second')
+        tbl.text('second_subscribers')
+        tbl.text('second_description')
+        tbl.text('second_url')
+        tbl.text('third')
+        tbl.text('third_subscribers')
+        tbl.text('third_description')
+        tbl.text('third_url')
     })
 };
 
