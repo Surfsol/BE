2619a309603c66558791fae098b5f3b4192fb302
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-20 16:04:49 -0800

    Added more tests, still not completed but framework is there

--- routes-models/posts/posts.spec.js
+++ routes-models/posts/posts.spec.js
@@ -4,14 +4,65 @@ const server = require('../../api/server')
 
 const db = require('../../data/db-config.js')
 
-describe('This is to verify we are testing', () => {
+describe('This is to set up our test environment', () => {
     beforeEach(async () => {
         await db('users').truncate();
-        await db('subreddits').truncate();
+        await db('subreddit').truncate();
         await db('posts').truncate();
         await request(server).post('/users/register').send({username: "Jest", password: "pass"})
+        const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
+        await request(server).set('Authorization', login.body.token)
     })
-    it('makes sure we are testing in a test env', () => {
-        expect(process.env.DB_ENV).toBe('test')
+})
+
+describe('This is to verify we are logged on', () => {
+    it('gets an OK and status code of 200', async () => {
+        const response = await request(server).get('/post/')
+        expect(response.status).toBe(200)
+    })
+    it('gets an array', async () => {
+        await db('posts').truncate();
+        const response = await request(server).get('/post/')
+        expect(response.text).toBe('[]')
+    })
+})
+
+describe('This is to add a new post', () => {
+    it('gets a CREATED and status code of 201', async () => {
+        await db('posts').truncate();
+        const response = await request(server).post('/post/new').send({
+            title: 'Test', 
+            content: 'Test Content', 
+            user_id: 1, 
+            link: 0
+        })
+        expect(response.status).toBe(201)
+    })
+    it('gets an object?', async () => {
+        await db('posts').truncate();
+        const response = await request(server).post('/post/new').send({
+            title: 'Test', 
+            content: 'Test Content',
+            user_id: 1, 
+            link: 0
+        })
+        // console.log("Object?", response)
+        // console.log("Object?", JSON.stringify(response.body))
+        console.log("Body", )
+        expect(response.body).toBe(JSON.stringify(`${"post:" [{
+            id: 5, 
+            title: 'Test', 
+            content: 'Test Content', 
+            user_id: 1, link: 0, 
+            first: null, 
+            first_subscribers: null, 
+            first_description: null,
+            second: null,
+            second_subscribers: null,
+            second_description: null,
+            third: null,
+            third_subscribers: null,
+            third_description: null,
+            subreddit_id: null}]}`))
     })
 })
--- routes-models/subreddit/sub.spec.js
+++ routes-models/subreddit/sub.spec.js
@@ -1,32 +1,36 @@
-const request = require('supertest')
+// const request = require('supertest')
 
-const server = require('../../api/server')
+// const server = require('../../api/server')
 
-const db = require('../../data/db-config.js')
+// const db = require('../../data/db-config.js')
 
-describe('This is to verify we are testing', () => {
-    beforeEach(async () => {
-        await db('users').truncate();
-        await request(server).post('/users/register').send({username: "Jest", password: "pass"})
-    })
-    it('makes sure we are testing in a test env', () => {
-        expect(process.env.DB_ENV).toBe('test')
-    })
-})
+// describe('This is to set up our test environment', () => {
+//     beforeEach(async () => {
+//         await db('users').truncate();
+//         await db('subreddit').truncate();
+//         await db('posts').truncate();
+//         await request(server).post('/users/register').send({username: "Jest", password: "pass"})
+//         // const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
+//         // await request(server).set('Authorization', login.body.token)
+//     })
+// })
 
-describe('This is testing the subreddit route', () => {
-    describe('This gets all of the subreddits', () => {
-        it('gets a response status of 200', async () => {
-            const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
-            const response = await request(server).get('/subreddit').set('Authorization', login.body.token)
-            expect(response.status).toBe(200)
-        })
-        it('should return a piece of JSON', async () => {
-            const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
-            const response = await request(server).get('/subreddit').set('Authorization', login.body.token)
-            expect(response.body.isArray).toBe(true)
-            // expect(response.body).toHaveProperty('id')
-            // expect(response.body).toHaveProperty('title')
-        })
-    })
-})
+// describe('This is testing the subreddit route', () => {
+//     describe('This gets all of the subreddits', () => {
+//         it('gets a response status of 200', async () => {
+//             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
+//             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
+//             expect(response.status).toBe(200)
+//         })
+//         it('should return an array', async () => {
+//             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
+//             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
+//             expect(JSON.stringify(response.body)).toMatch(`${[]}`)
+//         })
+//         it('should return a piece of JSON', async () => {
+//             const login = await request(server).post('/users/login').send({username: "Jest", password: "pass"})
+//             const response = await request(server).get('/subreddit/').set('Authorization', login.body.token)
+//             expect(response.type).toMatch(/json/i)
+//         })
+//     })
+// })
